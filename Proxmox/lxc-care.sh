de5e8a8536eae4ca831c80edd0ce19e0fb0b895d
CK <ck@lanasutra.de>
2024-05-13 10:36:46 +0200

    Changes

--- Proxmox/lxc-care.sh
+++ Proxmox/lxc-care.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# curl -sL https://raw.githubusercontent.com/ScriptSchublade/LazyScripts/main/Proxmox/lxc-care.sh | bash
 /usr/sbin/pct start $1
 pct exec $1 -- bash -c 'apt-get update && apt-get dist-upgrade -y && apt autoremove --purge && apt-get clean -y'
 /usr/sbin/pct stop $1
