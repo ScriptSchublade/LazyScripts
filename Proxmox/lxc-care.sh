e86ab1724e0184c7063e29fc92eb592f55b08561
CK <ck@lanasutra.de>
2024-05-13 10:44:56 +0200

    changes

--- Proxmox/lxc-care.sh
+++ Proxmox/lxc-care.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # curl -sL https://raw.githubusercontent.com/ScriptSchublade/LazyScripts/main/Proxmox/lxc-care.sh | bash
-echo "Starting $1"
-/usr/sbin/pct start $1
-pct exec $1 -- bash -c 'apt-get update && apt-get dist-upgrade -y && apt autoremove --purge && apt-get clean -y'
-/usr/sbin/pct stop $1
+echo "Starting: $1";
+/usr/sbin/pct start $1;
+pct exec $1 -- bash -c 'apt-get update && apt-get dist-upgrade -y && apt autoremove --purge && apt-get clean -y';
+/usr/sbin/pct stop $1;
