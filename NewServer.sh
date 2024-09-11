5460052199d9400ac0da8402eb64884740c25ca7
ScriptSchublade <ck@lanasutra.de>
2024-09-11 15:39:26 +0200

    Update NewServer.sh

--- NewServer.sh
+++ NewServer.sh
@@ -9,7 +9,7 @@ swapon /swapfile
 echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 echo 'vm.swappiness = 10' | sudo tee -a /etc/sysctl.conf
 echo 'vm.vfs_cache_pressure=50' | sudo tee -a /etc/sysctl.conf
-apt-get install -y curl mc htop ca-certificates cron
+apt-get install -y curl mc htop ca-certificates cron sudo
 curl -fsSL https://gist.githubusercontent.com/vsefer/f2696e997e1ab4316a50/raw/78544b83cb85428ba057fb02f8bbdd2bae7681db/htz-bashrc -o /root/.bashrc
 (crontab -l 2>/dev/null; echo "45 4 * * * apt-get update && apt-get dist-upgrade -y && apt autoremove -y --purge && apt-get clean -y") | crontab -
 
